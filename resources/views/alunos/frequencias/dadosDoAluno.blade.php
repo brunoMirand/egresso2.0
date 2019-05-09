b8fe6a04c9f0af7c10d5d4595fb36cebff8f62bc
Bruno Miranda <bruno.silva@catho.com>
2019-05-09 10:44:53 -0300

    feat[BR]: add image and student course in template

--- resources/views/alunos/frequencias/dadosDoAluno.blade.php
+++ resources/views/alunos/frequencias/dadosDoAluno.blade.php
@@ -1,4 +1,6 @@
 @php ($nome = $dados['dadosDoAluno']['nome'])
+@php ($foto = $dados['dadosDoAluno']['foto'])
+@php ($curso = $dados['dadosDoAluno']['cursos_id'])
 <div class="col-md-4">
     <div class="card">
         <div class="card-header">
@@ -7,10 +9,10 @@
         </div>
         <div class="card-body">
             <div class="mx-auto d-block">
-                <img class="rounded-circle mx-auto d-block" src="#" alt="Foto do aluno">
+                <img class="rounded-circle mx-auto d-block" src='data:image/png;base64,{{ $foto }}' alt="Foto do aluno">
                 <h5 class="text-sm-center mt-2 mb-1">{{ $nome }}</h5>
                 <div class="location text-sm-center">
-                    <i class="fa fa-map-marker"></i> Curso</div>
+                    <i class="fa fa-map-marker"></i> {{ $curso }}</div>
             </div>
             <hr>
         </div>
